bac153d6dd034e0dea3b2bb970542f0c985f5350
IcyHubStudios <160991308+IcyHubStudios@users.noreply.github.com>
2024-02-22 22:49:01 -0500

    Fix discord_command.lua
    I typed something, if there are any errors, please let me know.

--- discord_command/discord_command.lua
+++ discord_command/discord_command.lua
@@ -1,7 +1,15 @@
-RegisterCommand("discord", function(source, args, rawCommand)
+RegisterCommand("discord", function(source, args, rawCommand) --"discord" is the /command in game. You may change it
     TriggerEvent('chat:addMessage', {
         color = {255, 255, 255},
         multiline = true,
-        args = {"^1[System]", "Join our Discord server at discord.gg/yourserver!"}
+        args = {"^1[System]", "Join our Discord server at discord.gg/example!"} --your discord link. You may change the [System] to something else.
     })
 end, false)
+
+
+-- Add another command? Copy everything above this and customize it how you like it. 
+######################################################
+#                                                    #
+#   https://github.com/IcyHubStudios/chat-commands   #
+#                 IcyHubStudios                      #
+######################################################
